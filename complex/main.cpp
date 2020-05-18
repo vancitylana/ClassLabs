835f45e71a581f32dcbb35bb005592f14c1b99ca
vancitylana <58149793+vancitylana@users.noreply.github.com>
2020-05-18 19:05:30 +0300

    Update main.cpp

--- complex/main.cpp
+++ complex/main.cpp
@@ -4,11 +4,36 @@
 
 int main()
 {
-    Complex a(0.25, 0.25);
-    Complex b(a);
-    Complex c = a + b;
-    c.print_trig(std::cout);
-    std::cout << "\n";
+    Complex a;
+    Complex b(2,1);
+    Complex c(b);
+    std::cout<<"A= "<<a<<" B= "<<b<<" C= "<<c<<'\n';
+    Complex d = b+c;
+    std::cout<<"B + C = ";
+    std::cout<<d<<'\n';
+    d = b-c;
+    std::cout<<"B - C = ";
+    std::cout<<d<<'\n';
+    d = b*c;
+    std::cout<<"B * C = ";
+    std::cout<<d<<'\n';
+    d = b/c;
+    std::cout<<"B / C = ";
+    std::cout<<d<<'\n';
+    std::cout<<"B == C = ";
+    std::cout<<static_cast<int>(b==c)<<'\n';
+    d=a;
+    std::cout<<"D = ";
+    std::cout<<d<<'\n';
+    std::cout<<"|B| = ";
+    std::cout<<b.module()<<'\n';
+    std::cout<<"Trig. B: ";
+    b.print_trig(std::cout);
+    std::cout<<'\n';
+    b.pow(b);
+    std::cout<<"B ^ B = ";
+    std::cout<<b<<'\n';
+
 
     return 0;
 }
